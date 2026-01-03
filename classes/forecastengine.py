7fc3812dcac5c44bc74779771eceb1c02e14a818
Rflores45175 <RudyFlores60@gmail.com>
2026-01-03 05:32:03 -0600

    created forecastengine.py skeleton.

--- classes/forecastengine.py
+++ classes/forecastengine.py
@@ -1,4 +1,16 @@
+from datetime import date, timedelta
+
 class ForecastEngine:
-    def __init__(self):
+    def __init__(self, budget_manager, lookback_days=30):
+        self.budget_manager = budget_manager
+        self.lookback_days = lookback_days
+
+    def average_daily_net(self):
+        pass
+
+    def forecast_balance(self):
+        pass
+
+    def days_until_zero(self):
         pass
 
